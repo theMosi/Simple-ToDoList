9ef474c8d5a0ecf67695200ee02e5ed6fd2ffa45
Mosi <mostafawshe@gmail.com>
2023-02-13 15:19:43 +0330

    update task comp

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import router from './router.js';
 import store from './store/index.js';
 
